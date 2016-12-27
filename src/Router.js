8026ac57a4057efff60506291b0a10e5d8be81c2
Anh Tuan Nguyen <anhtuank7c@hotmail.com>
2016-12-28 06:17:48 +0700

    Handler backAndroid

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { AsyncStorage } from 'react-native';
+import React, { Component } from 'react';
+import { AsyncStorage, BackAndroid, Platform, ToastAndroid } from 'react-native';
 import { Actions, Router, Scene, Reducer } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 
@@ -19,111 +19,132 @@ import NavigationDrawer from './components/NavigationDrawer';
 
 import { Spinner, TabIcon } from './components/common';
 
-const socket = io('http://albums.crabstudio.info', {
-    transports: ['websocket'],
-    forceNew: true
-});
+class RouterComponent extends Component {
+    state = {
+        backPress: false
+    }
 
-const app = feathers()
-    .configure(socketio(socket))
-    .configure(hooks())
-    .configure(authentication({
-        storage: AsyncStorage
-    }));
+    componentWillMount() {
+        const socket = io('http://albums.crabstudio.info', {
+            transports: ['websocket'],
+            forceNew: true
+        });
 
-app.io.on('connect', () => console.log('Socket connected'))
-    .on('disconnect', () => console.log('Socket disconnected'));
+        this.app = feathers()
+            .configure(socketio(socket))
+            .configure(hooks())
+            .configure(authentication({
+                storage: AsyncStorage
+            }));
 
-const reducerCreate = (params) => (state, action) => Reducer(params)(state, action);
+        this.app.io.on('connect', () => console.log('Socket connected'))
+            .on('disconnect', () => console.log('Socket disconnected'));
+    }
 
-const RouterComponent = ({ loading, needSignIn }) => (
-    loading ?
-        <Spinner size="large" /> :
-        <Router createReducer={reducerCreate}>
+    render() {
+        const reducerCreate = (params) => (state, action) => Reducer(params)(state, action);
+        const { loading, needSignIn } = this.props;
+        return (
+            loading ?
+                <Spinner size="large" /> :
+                <Router
+                    createReducer={reducerCreate}
+                    backAndroidHandler={() => {
+                        if (!this.state.backPress) {
+                            ToastAndroid.show('Press Back again to exit', ToastAndroid.SHORT);
+                            this.setState({ backPress: true });
+                            setTimeout(() => this.setState({ backPress: false }), 2500);
+                        } else {
+                            return false;
+                        }
+                        return true;
+                    } }>
 
-            <Scene key="auth">
-                <Scene
-                    key="sign_in"
-                    component={SignIn}
-                    app={app}
-                    initial={needSignIn}
-                    title="Sign in"
-                    type="reset"
-                    />
-                <Scene
-                    key="sign_up"
-                    component={SignUp}
-                    app={app}
-                    title="Sign up"
-                    type="replace"
-                    leftTitle="Back"
-                    onLeft={() => Actions.sign_in({ type: 'replace' })}
-                    sceneStyle={{ paddingTop: 65 }}
-                    />
-            </Scene>
+                    <Scene key="auth">
+                        <Scene
+                            key="sign_in"
+                            component={SignIn}
+                            app={this.app}
+                            initial={needSignIn}
+                            title="Sign in"
+                            type="reset"
+                            />
+                        <Scene
+                            key="sign_up"
+                            component={SignUp}
+                            app={this.app}
+                            title="Sign up"
+                            type="replace"
+                            leftTitle="Back"
+                            onLeft={() => Actions.sign_in({ type: 'replace' })}
+                            sceneStyle={{ paddingTop: 65 }}
+                            />
+                    </Scene>
 
-            <Scene
-                key="drawer"
-                component={NavigationDrawer}
-                initial={!needSignIn}
-                hideNavBar
-                unmountScenes
-                open={false}
-                >
-                <Scene
-                    key="main"
-                    tabs
-                    tabBarStyle={styles.tabBarStyle}
-                    tabBarSelectedItemStyle={styles.tabBarSelectedItemStyle}
-                    >
                     <Scene
-                        key="news_list"
-                        component={NewsList}
-                        title="News"
-                        app={app}
-                        sceneStyle={{ marginTop: 65 }}
-                        icon={TabIcon}
-                        iconName="wifi-tethering"
-                        iconSize={24}
-                        />
-                    <Scene
-                        key="lucky_number"
-                        component={NewsList}
-                        title="Random"
-                        app={app}
-                        navigationBarStyle={{ backgroundColor: 'green' }}
-                        sceneStyle={{ marginTop: 65 }}
-                        icon={TabIcon}
-                        iconName="location-searching"
-                        iconSize={24}
-                        />
-                    <Scene
-                        key="prize"
-                        component={NewsList}
-                        title="Prize"
-                        app={app}
-                        navigationBarStyle={{ backgroundColor: 'yellow' }}
-                        sceneStyle={{ marginTop: 65 }}
-                        icon={TabIcon}
-                        iconName="trending-up"
-                        iconSize={24}
-                        />
-                    <Scene
-                        key="history"
-                        component={NewsList}
-                        title="History"
-                        app={app}
-                        navigationBarStyle={{ backgroundColor: 'yellow' }}
-                        sceneStyle={{ marginTop: 65 }}
-                        icon={TabIcon}
-                        iconName="history"
-                        iconSize={24}
-                        />
-                    <Scene key="profile" component={Profile} title="Profile" />
-                </Scene>
-            </Scene>
-        </Router>
-);
+                        key="drawer"
+                        component={NavigationDrawer}
+                        initial={!needSignIn}
+                        hideNavBar
+                        unmountScenes
+                        open={false}
+                        >
+                        <Scene
+                            key="main"
+                            tabs
+                            tabBarStyle={styles.tabBarStyle}
+                            tabBarSelectedItemStyle={styles.tabBarSelectedItemStyle}
+                            >
+                            <Scene
+                                key="news_list"
+                                component={NewsList}
+                                title="News"
+                                app={this.app}
+                                sceneStyle={{ marginTop: 65 }}
+                                icon={TabIcon}
+                                iconName="wifi-tethering"
+                                iconSize={24}
+                                />
+                            <Scene
+                                key="lucky_number"
+                                component={NewsList}
+                                title="Random"
+                                app={this.app}
+                                navigationBarStyle={{ backgroundColor: 'green' }}
+                                sceneStyle={{ marginTop: 65 }}
+                                icon={TabIcon}
+                                iconName="location-searching"
+                                iconSize={24}
+                                />
+                            <Scene
+                                key="prize"
+                                component={NewsList}
+                                title="Prize"
+                                app={this.app}
+                                navigationBarStyle={{ backgroundColor: 'yellow' }}
+                                sceneStyle={{ marginTop: 65 }}
+                                icon={TabIcon}
+                                iconName="trending-up"
+                                iconSize={24}
+                                />
+                            <Scene
+                                key="history"
+                                component={NewsList}
+                                title="History"
+                                app={this.app}
+                                navigationBarStyle={{ backgroundColor: 'yellow' }}
+                                sceneStyle={{ marginTop: 65 }}
+                                icon={TabIcon}
+                                iconName="history"
+                                iconSize={24}
+                                />
+                            <Scene key="profile" component={Profile} title="Profile" />
+                        </Scene>
+                    </Scene>
+                </Router>
+        );
+    }
+}
 
 // props is props of this component transfered from parent component
 const mapStateToProps = (state) => {
